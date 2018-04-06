b5591f9218541b8858ad85867f5131fc84d5fcf3
Paul Vermeer <paulvermeer@gmail.com>
2018-04-06 13:51:18 +0200

    ens hotfix: tested error reporting

--- src/shared/helpers/error.helper.ts
+++ src/shared/helpers/error.helper.ts
@@ -1,40 +1,41 @@
 import { IError } from "../interfaces";
 
-class ErrorMessagesHelper {	
+export class ErrorMessages {	
 
 	static returnUnspecifiedError():IError {
-		return this.getType('9999');
+		return this.getErrorMessage(9999);
 	}
 
-	static buildErrorMessage (errorNumber:number, done:Function):IError {		
+	static getErrorMessage (errorNumber:number, done?:Function):IError {		
 
-		let error:IError;
+		let error:IError, 
+			errorType = errorNumber.toString();		
 
-		// extract numbers from string
-		let errorType = errorNumber.match(/\d/g);		
-		errorType = String(errorType);
-
-		if(!errorType || (errorType && typeof errorType != "string") || (errorType && errorType.length < 4) ) {
+		if(!errorType || (errorType && typeof errorType != "string") || (errorType && errorType.length < 4) ) {		
 			error = this.returnUnspecifiedError();		
 		} else {
 			error = this.getType(errorType);
 		}
 
-		return done(error);	
+		if(done && typeof done === 'function') {
+			return done(error);	
+		} else {
+			return error;
+		}		
 	}
 
 	static getType(errorType:string):IError {	
 		console.log('==> processing error ', errorType);
 		let error:IError;
-		switch(type) {			
+		switch(errorType) {			
 
 			// default error message
+			case '9999':		
 			default: error = { type: 9999, message: 'An unexpected error has occured. The issue has been logged and will be investigated by our engineers.'}; break;
 		}
 
-		return;
+		return error;
 	}	
-
 }
 
 
