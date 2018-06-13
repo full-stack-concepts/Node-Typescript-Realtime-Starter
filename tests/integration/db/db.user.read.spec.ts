68fd12fa125cbf1069f09a13bca256afef94faf8
Paul Vermeer <paulvermeer@gmail.com>
2018-06-13 14:23:18 +0200

    ens-testing: default tests for user read model and associated repo

--- tests/integration/db/db.user.read.spec.ts
+++ tests/integration/db/db.user.read.spec.ts
@@ -12,6 +12,8 @@ import {
 	ReadWriteRepository
 } from "../helpers/";
 
+import { IUser} from "../../../src/shared/interfaces";
+
 /****
  *
  */
@@ -27,8 +29,13 @@ var methods:any;
 var connection:any;
 var redisClient:any;
 var repository:any;
+const userRole:number = 5;
 
-describe("User Default Database Read Actions ", () => {
+describe("Database User Read Model Methods", () => {
+
+	let err:any;
+	let users:IUser[];
+	let user:IUser;
 
 	before( async () => {
 
@@ -45,24 +52,91 @@ describe("User Default Database Read Actions ", () => {
 		repository= new ReadRepository('User', connection, redisClient);	
 		
 		// import repository into our test model
-		testModel.repo = repository;		
+		testModel.repo = repository;	
 
 	});
 
-	it("shoud do something for now", async () => {	
+	it("shoud create an intance of ReadRepository", async () => {	
 
-		expect(1).to.equal(1);
+		let testRepo:any = new ReadRepository('User', connection, redisClient);	
+
+		expect(testRepo).to.be.an("object");
+		expect(testRepo).to.be.an.instanceOf(ReadRepository);
+	
+	});
+
+	/****
+	 *
+	 */
+	describe(" READ / findAll ", async() => {		
+
+		before( async () => {
+			try { users = await testModel.findAll(); }
+			catch(e) {  err=e; }
+		});
+
+		it("return an array of users without error", async () => {				
+			expect(err).to.be.undefined;
+			expect(users).to.be.an("array");
+			expect(users.length).to.be.gte(0);			
+		});
+
+	});
+
+	/****
+	 * 
+	 */
+	describe(" READ / findOne ", async() => {
+
+		let err:any;
+		let user:IUser;
+		let identifier:string;
+
+		before( async() => {
+			if(users[0]) {
+				
+				identifier = users[0].core.email;
+				try { user = await testModel.findOne({ 'core.email':identifier}); }
+				catch(e) {  err=e; }
+
+			} else {
+				this.skip()
+			}
+		});
+
+		it("find user without error", async () => {		
+
+			expect(err).to.be.undefined;
+			expect(user).to.be.an("object");
+			expect(user.core).to.have.property('email');			
+		});		
+	});
+
+	/****
+	 * 
+	 */
+	describe(" READ / find ", async() => {
+
+		before( async() => {				
+			try { users = await testModel.find({ 'core.role':userRole}); }
+			catch(e) {  err=e; }		
+		});
+
+		it("return an array of users without error", async () => {				
+			expect(err).to.be.undefined;
+			expect(users).to.be.an("array");
+			expect(users.length).to.be.gte(0);			
+		});
+
 	});
 
 	after( async () => {
 
 		// close db connection
 		dbTestEnvironment.closeConnection();
-
-
-	})
-
-
+	
+	});
+	
 })
 
 
